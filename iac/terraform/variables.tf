40360bc946af2479fe19435d17fcc8415ae696ab
Ashish1806xx <ashishdaida01@gmail.com>
2025-08-10 16:23:33 -0400

    Update variables.tf

--- iac/terraform/variables.tf
+++ iac/terraform/variables.tf
@@ -2,3 +2,21 @@ variable "image" {
   description = "Fully qualified image tag"
   type        = string
 }
+
+variable "container_name" {
+  description = "Container name"
+  type        = string
+  default     = "jenkinslab-app"
+}
+
+variable "host_port" {
+  description = "Host port to expose"
+  type        = number
+  default     = 8082
+}
+
+variable "container_port" {
+  description = "Container internal port"
+  type        = number
+  default     = 8080
+}
