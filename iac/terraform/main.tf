fb75c919b6a63672a45074b38b1234b6b001bff9
Ashish1806xx <ashishdaida01@gmail.com>
2025-08-10 16:23:14 -0400

    Update main.tf

--- iac/terraform/main.tf
+++ iac/terraform/main.tf
@@ -1,14 +1,27 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {}
+
 resource "docker_image" "app" {
   name         = var.image
   keep_locally = true
 }
 
 resource "docker_container" "app" {
-  name  = "jenkinslab-app"
+  name  = var.container_name
   image = docker_image.app.image_id
 
   ports {
-    internal = 8080
-    external = 8082
+    internal = var.container_port
+    external = var.host_port
   }
+
+  restart = "unless-stopped"
 }
