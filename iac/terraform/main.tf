3ed1986894c7501856b45c936be4997ce2cea0c7
Ashish1806xx <ashishdaida01@gmail.com>
2025-08-10 16:31:33 -0400

    Update main.tf

--- iac/terraform/main.tf
+++ iac/terraform/main.tf
@@ -1,27 +1,19 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 3.0"
-    }
-  }
-}
-
-provider "docker" {}
-
+# Build reference to the already-built image
 resource "docker_image" "app" {
   name         = var.image
   keep_locally = true
 }
 
+# Run the container
 resource "docker_container" "app" {
   name  = var.container_name
   image = docker_image.app.image_id
 
+  restart = "unless-stopped"
+
   ports {
     internal = var.container_port
     external = var.host_port
+    protocol = "tcp"
   }
-
-  restart = "unless-stopped"
 }
